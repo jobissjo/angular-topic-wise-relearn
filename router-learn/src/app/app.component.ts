4858f3413dd9f9e2515691467598cdf4f02d2678
Jobi S S <jobisjobi1234@gmail.com>
2024-01-08 20:43:38 +0530

    completed learning of basic concept of routing learning

--- router-learn/src/app/app.component.ts
+++ router-learn/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'router-learn';
+
+  showLoader:boolean = false;
+
+  constructor(private router:Router) {}
+
+  ngOnInit(){
+
+    this.router.events.subscribe((routerEvent:Event) => {
+      if (routerEvent instanceof NavigationStart){
+        this.showLoader = true;
+      }
+      if(routerEvent instanceof NavigationEnd || routerEvent instanceof NavigationCancel
+        || routerEvent instanceof NavigationError){
+          
+        this.showLoader = false;
+      }
+    })
+  }
+  
 }
--- router-learn/src/app/components/checkout/checkout.component.ts
+++ router-learn/src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Course } from 'src/app/Models/course';
 
 @Component({
   selector: 'app-checkout',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent {
+  constructor(private router:Router) {}
+  course : any;
+  ngOnInit(){
+    this.course = this.router.lastSuccessfulNavigation?.extras.state;
+  }
 
 }
