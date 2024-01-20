e807c1d0279ff0f09bcc2835aae0a667147b6cda
Jobi S S <jobisjobi1234@gmail.com>
2024-01-20 22:55:35 +0530

    mutate method in signals

--- learn-signals/src/app/components/signals/signals.component.ts
+++ learn-signals/src/app/components/signals/signals.component.ts
@@ -8,19 +8,26 @@ import { Component, signal } from '@angular/core';
 export class SignalsComponent {
   counter = signal(0);
 
-  message: string[] = [];
+  // message: string[] = [];
+  message = signal<string[]>([])
 
   increment(){
-  //  this.counter.set( this.counter() + 1);
+  
   this.counter.update(()=> this.counter() + 1);
-   this.message.push('Current counter value is: ' + this.counter());
+  // this.message.update((prevMsg)=> [...prevMsg, 'Current counter value is: ' + this.counter()]);
+
+  this.message.mutate((prevMsg)=> prevMsg.push('Current counter value is: ' + this.counter()))
+
+  //  this.counter.set( this.counter() + 1);
+  //  this.message.push('Current counter value is: ' + this.counter());
+  
   }
 
   decrement(){
     if (this.counter() > 0){
       // this.counter.set(this.counter() - 1);
       this.counter.update(()=> this.counter() + 1)
-      this.message.pop();
+      this.message.mutate((preMsg)=> preMsg.pop());
     }
   }
 }
